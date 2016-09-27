9442d3843c88da614131fa90db64e8f8c73ff6cb
alexey109 <alexey109@gmail.com>
2016-09-28 00:29:18 +0300

    V1.2 Code refactoring. Split to modules, rewrite using classes. Improve stability.

---
+++ parser.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+import xlrd
+import re
+import datetime as dt
+
+
+# This class present schedule, wich store like regural excel-file. 
+# Now it can only show lection for special parameters, but in future
+# it'll could load ecxel document into database and work with it,
+# also new functions will be add like "when next lesson start" and etc.
+class schedule:
+
+	# Dictionary of lections start-end time
+	# Return type: string
+	timeFromLection = {
+		1: '9:00-10:35',
+		2: '10:45-12:20',
+		3: '12:50-14:25',
+		4: '14:35-16:10',
+		5: '16:20-17:55',
+		6: '18:00-21:20',
+	}
+	
+	# Return row number of start and end for a day of week in sheet
+	# Return type: list of (start, end)
+	rowFromDay = {
+		0: (4,8),
+		1: (9,15),
+		2: (16,20),
+		3: (21,27),
+		4: (28,33),
+		5: (34,39)
+	}
+
+	
+	# Open document by xlrd module
+	def __init__(self):
+		self.xls_sheet = xlrd.open_workbook("it-2k.-16_17-osen.xls").sheet_by_index(0)
+
+	# Check if lection frequency match specified week number,
+	# it mean that function returns true if lection have to be on that week.
+	# Return type: boolean
+	def isThatWeek(self, frequency, week_numb):
+		week_numb = week_numb - dt.date(2016, 9, 1).isocalendar()[1] + 1
+	
+		if frequency == 'all':
+			result = True
+		elif 'I' in frequency:
+			result = (week_numb % 2 == 0) == (frequency.strip() == 'II')
+		else:
+			result = str(week_numb) in re.split(r'[\s,]', frequency)
+	
+		return result
+
+	# Get frequency value from cell with lections name.
+	# Return type: list of (lection_name, frequency)
+	def getFrequency(self, cell_value):
+		content = []
+		frequencies = []
+
+		for text in re.split(u'[\d\s,I]*\sн\.', cell_value):
+			if text:
+				content.append(text)
+
+		found_frq = re.findall(u'[\d\s,I]*\sн\.', cell_value)
+		for i, frequency in enumerate(found_frq):
+			frequencies.append([content[i], re.sub(u'[\.\sн\n]', '',frequency)])
+
+		if not found_frq:	
+			frequencies = [[cell_value, 'all']]	
+
+		return frequencies
+
+	# Get classrooms values from cell with classroom
+	# Return type: list of strings
+	def getClassroom(self, cell_value):
+		if not cell_value:
+			return ['-']
+		return re.findall(u'[А-Яа-я]*-[\dА-Яа-я]*', cell_value)
+	
+
+	# Primary function, that returns lection timetable from timetable document for special parametrs.
+	# Return type: string
+	def getLectionForGroup(self, group_name, day_of_week, week_numb, lection_start = 0):
+		row_start = self.rowFromDay[day_of_week][0]	
+		row_end   = self.rowFromDay[day_of_week][1]
+
+		group_colx = 0 
+		while (self.xls_sheet.cell_value(rowx = 2, colx = group_colx) != group_name)\
+		and(group_colx < self.xls_sheet.ncols) :
+			group_colx += 1
+
+		timetable  = ''
+		for row_idx in range(row_start, row_end):
+			text_cell_value = self.xls_sheet.cell_value(rowx = row_idx, colx = group_colx)
+			if text_cell_value:
+				lection_numb = self.xls_sheet.cell_value(rowx = row_idx, colx = 2)[0]
+				classroom = self.getClassroom(self.xls_sheet.cell_value(rowx = row_idx, colx = group_colx+1))
+				frequency = self.getFrequency(text_cell_value)
+				for i, lection in enumerate(frequency):
+					if self.isThatWeek(lection[1], week_numb) and int(lection_numb) >= int(lection_start):
+						timetable += u'\n%.5s пара (%.5s, %.11s):\n %.100s\n' % \
+						(lection_numb, classroom[i], self.timeFromLection[int(lection_numb)],lection[0].strip())
+	
+		return timetable if timetable else u'\nпар нет :)'
+
--- timebot.py
+++ timebot.py
@@ -1,208 +1,168 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-
-import xlrd
-import re
 import datetime as dt
 import time
 import string
 import vk
 import sys
-from random import randint
 
-# Dictionary of lections start-end time
-# Return type: string
-timeFromLection = {
-	1: '9:00-10:35',
-	2: '10:45-12:20',
-	3: '12:50-14:25',
-	4: '14:35-16:10',
-	5: '16:20-17:55',
-	6: '18:00-21:20',
-}
-
-# Return row number of start and end for a day of week in sheet
-# Return type: list of (start, end)
-rowFromDay = {
-	0: (4,8),
-	1: (9,15),
-	2: (16,20),
-	3: (21,27),
-	4: (28,33),
-	5: (34,39)
-}
-
-# Return lection number for specified time
-# Result type: integer
-def lectionFromTime(dt_time):
-	return {
-							dt_time < dt.time(9,0,0): 0,
-		dt.time(9,0,0) 	 <= dt_time < dt.time(10,45,0): 1,
-		dt.time(10,45,0) <= dt_time < dt.time(12,50,0): 2,
-		dt.time(12,50,0) <= dt_time < dt.time(14,35,0): 3,
-		dt.time(14,35,0) <= dt_time < dt.time(16,20,0): 4,
-		dt.time(16,20,0) <= dt_time < dt.time(18,0,0): 5,
-		dt.time(18,0,0)  <= dt_time < dt.time(21,20,0): 6,
-		dt.time(21,20,0) <= dt_time: 7
-	}[True]
-
-# Check if lection frequency match specified week number,
-# it mean that function returns true if lection have to be on that week.
-# Result type: boolean
-def isThatWeek(frequency, week_numb):
-	week_numb = week_numb - dt.date(2016, 9, 1).isocalendar()[1] + 1
-	
-	if frequency == 'all':
-		result = True
-	elif 'I' in frequency:
-		result = (week_numb % 2 == 0) == (frequency.strip() == 'II')
-	else:
-		result = str(week_numb) in re.split(r'[\s,]', frequency)
-	
-	return result
-
-# Get frequency value from cell with lections name.
-# Result type: list of (lection_name, frequency)
-def getFrequency(cell_value):
-	content = []
-	frequencies = []
-
-	for text in re.split(u'[\d\s,I]*\sн\.', cell_value):
-		if text:
-			content.append(text)
-
-	found_frq = re.findall(u'[\d\s,I]*\sн\.', cell_value)
-	for i, frequency in enumerate(found_frq):
-		frequencies.append([content[i], re.sub(u'[\.\sн\n]', '',frequency)])
-
-	if not found_frq:	
-		frequencies = [[cell_value, 'all']]	
-
-	return frequencies
-
-# Get classrooms values from cell with classroom
-# Result type: list of strings
-def getClassroom(cell_value):
-	if not cell_value:
-		return ['-']
-	return re.findall(u'[А-Яа-я]*-[\dА-Яа-я]*', cell_value)
-	
-
-# Primary function, that returns lection timetable from timetable document for special parametrs.
-# Result type: string
-def getLectionForGroup(group_name, day_of_week, week_numb, lection_start = 0):
-	xls_sheet = xlrd.open_workbook("it-2k.-16_17-osen.xls").sheet_by_index(0)
-	row_start = rowFromDay[day_of_week][0]	
-	row_end   = rowFromDay[day_of_week][1]
-
-	group_colx = 0 
-	while (xls_sheet.cell_value(rowx = 2, colx = group_colx) != group_name) and \
-	(group_colx < xls_sheet.ncols) :
-		group_colx += 1
-
-	timetable  = ''
-	for row_idx in range(row_start, row_end):
-		text_cell_value = xls_sheet.cell_value(rowx = row_idx, colx = group_colx)
-		if text_cell_value:
-			lection_numb = xls_sheet.cell_value(rowx = row_idx, colx = 2)[0]
-			classroom = getClassroom(xls_sheet.cell_value(rowx = row_idx, colx = group_colx+1))
-			frequency = getFrequency(text_cell_value)
-			for i, lection in enumerate(frequency):
-				if isThatWeek(lection[1], week_numb) and int(lection_numb) >= int(lection_start):
-					timetable += u'\n%.5s пара (%.5s, %.11s):\n %.100s\n' % \
-					(lection_numb, classroom[i], timeFromLection[int(lection_numb)],lection[0].strip())
-	
-	return timetable
+import parser 
 
 
-# Return tommorow lections
-# Result type: string
-def getTommorowLections(group_name):
-	day_of_week = (dt.datetime.today() +  + dt.timedelta(days=1)).weekday()
-	week_numb = (dt.datetime.now() + dt.timedelta(days=1)).isocalendar()[1]
+# Class implement bot logic. The main idea of this bot to help everyone with schedule.
+# Bot intergated to socialnet and like a friend can tell you what next lection you will have.
+# Normal documentation will be in future.
+class timebot():
 
-	return getLectionForGroup(group_name, day_of_week, week_numb)
+	def __init__(self):
+		self.api = self.openVkAPI()
+		self.schedule = parser.schedule()
 
-# Return after tommorow lections
-# Result type: string
-def getAfterTommorowLections(group_name):
-	day_of_week = (dt.datetime.today() + dt.timedelta(days=2)).weekday()
-	week_numb = (dt.datetime.now() + dt.timedelta(days=2)).isocalendar()[1]
-	return getLectionForGroup(group_name, day_of_week, week_numb)
+	# Return lection number for specified time
+	# Return type: integer
+	def lectionFromTime(self, dt_time):
+		return {
+								dt_time < dt.time(9,0,0): 0,
+			dt.time(9,0,0) 	 <= dt_time < dt.time(10,45,0): 1,
+			dt.time(10,45,0) <= dt_time < dt.time(12,50,0): 2,
+			dt.time(12,50,0) <= dt_time < dt.time(14,35,0): 3,
+			dt.time(14,35,0) <= dt_time < dt.time(16,20,0): 4,
+			dt.time(16,20,0) <= dt_time < dt.time(18,0,0): 5,
+			dt.time(18,0,0)  <= dt_time < dt.time(21,20,0): 6,
+			dt.time(21,20,0) <= dt_time: 7
+		}[True]
 
-# Return today lections
-# Result type: string
-def getTodayLections(group_name):
-	day_of_week = dt.datetime.today().weekday()
-	week_numb = dt.datetime.now().isocalendar()[1]
-	
-	return getLectionForGroup(group_name, day_of_week, week_numb)
-
-# Return today next lection
-# Result type: string
-def getNextLections(group_name):
-	week_numb = dt.datetime.now().isocalendar()[1]
-	lection_start = int(lectionFromTime(dt.datetime.now().time())) + 1
-	if lection_start == 7:
+	# Return tommorow lections
+	# Return type: string
+	def getTommorowLections(self, group_name):
 		day_of_week = (dt.datetime.today() +  + dt.timedelta(days=1)).weekday()
-	else:
+		week_numb = (dt.datetime.now() + dt.timedelta(days=1)).isocalendar()[1]
+
+		message = u'Пары завтра:\n\n'
+		message += self.schedule.getLectionForGroup(group_name, day_of_week, week_numb)
+		return message
+
+	# Return after tommorow lections
+	# Return type: string
+	def getAfterTommorowLections(self, group_name):
+		day_of_week = (dt.datetime.today() + dt.timedelta(days=2)).weekday()
+		week_numb = (dt.datetime.now() + dt.timedelta(days=2)).isocalendar()[1]
+		message = u'Пары послезавтра:\n\n'
+		message += self.schedule.getLectionForGroup(group_name, day_of_week, week_numb)
+		return message
+
+	# Return today lections
+	# Return type: string
+	def getTodayLections(self, group_name):
 		day_of_week = dt.datetime.today().weekday()
+		week_numb = dt.datetime.now().isocalendar()[1]
 	
-	return getLectionForGroup(group_name, day_of_week, week_numb, lection_start)
+		message = u'Пары сегодня:\n\n'
+		message += self.schedule.getLectionForGroup(group_name, day_of_week, week_numb)
+		return message
 
-def getLections(message):
-	group_name = u'ИКБО-04-15'
-	result = ''
-
-	if not((u'пары' in message) or (u'лекции' in message)):
-		return result
-	if u'дальше' in message:
-		result = u'Следующие пары:\n\n' + getNextLections(group_name)
-	elif u'сегодня' in message:
-		result = u'Пары сегодня:\n\n' + getTodayLections(group_name)
-	elif u'послезавтра' in message:
-		result = u'Пары послезавтра:\n\n' + getAfterTommorowLections(group_name)
-	elif u'завтра' in message:
-		result = u'Пары завтра:\n\n' + getTommorowLections(group_name)
+	# Return today next lection
+	# Return type: string
+	def getNextLections(self, group_name):
+		week_numb = dt.datetime.now().isocalendar()[1]
+		lection_start = int(self.lectionFromTime(dt.datetime.now().time())) + 1
+		if lection_start == 7:
+			day_of_week = (dt.datetime.today() +  + dt.timedelta(days=1)).weekday()
+		else:
+			day_of_week = dt.datetime.today().weekday()
 	
-	return result
+		message = u'Следующие пары:\n\n'
+		message += self.schedule.getLectionForGroup(group_name, day_of_week, week_numb, lection_start)
+		return message
 
-''' For debugging.
-if len(sys.argv) != 2:
-	sys.exit("Not enough args")
-message = str(sys.argv[1])
-print getLections(message.decode('utf-8'))
-'''
- 
-# Open vkAPI session
-success = False
-while not success:
-	try:
-		session = vk.AuthSession(app_id='5637421', user_login='+79296021208', user_password='timebot109', scope='4096')
-		api = vk.API(session)
-		success = True
-	except:
-		time.sleep(10)
+	# Retrun message that bot send back to user.
+	# Return type: string
+	def getLections(self, message):
+		group_name = u'ИКБО-04-15'
+		answer = ''
 
-# Scan enter messages and answer
-counter = 0
-while counter < 3600*24*4:
-	counter += 1
-	try:
-		response = api.messages.get(out=0, count=10, time_offset=20)
-	except:
-		response = []	
-	for message in response:
+		if not((u'пары' in message) or (u'лекции' in message)):
+			return answer
+		if u'дальше' in message or u'следующие' in message:
+			answer = self.getNextLections(group_name)
+		elif u'сегодня' in message:
+			answer = self.getTodayLections(group_name)
+		elif u'послезавтра' in message:
+			answer = self.getAfterTommorowLections(group_name)
+		elif u'завтра' in message:
+			answer = self.getTommorowLections(group_name)
+	
+		return answer
+
+	# Check element of tuple by index for existance
+	# Return type: boolean
+	def is_exist(self, tupl, index_name):
 		try:
-			if message['read_state'] == 0: 
-				lections = getLections(message['body'].lower())
-				if lections:
-					try:
-						api.messages.send(chat_id=message['chat_id'], message=lections)
-					except:
-						api.messages.send(user_id=message['uid'], message=lections)
-					time.sleep(1)
+			tmp = tupl[index_name]
+			result =  True
 		except:
+			result = False
+		return result
+
+	# Open vkAPI session
+	# Return type: vk.api object
+	def openVkAPI(self):
+		success = False
+		while not success:
+			try:
+				print 'Try to open new session.'
+				session = vk.AuthSession(
+					app_id='5637421', 
+					user_login='+79296021208', 
+					user_password='timebot109', 
+					scope='4096')
+				api = vk.API(session)
+				success = True
+				print 'New session opened.'
+			except:
+				print 'Error ocured when tried to open session!'
+				time.sleep(4)
+		return api
+
+	# Send answer for enter message
+	# Return type: string 
+	# TODO Add status code, or use boolean.
+	def sendAnswer(self, message):
+		print 'Got new message ' + str(dt.datetime.now()) 
+		result = ''
+
+		try:
+			answer = self.getLections(message['body'].lower())
+			if answer: 
+				if self.is_exist(message, 'chat_id'):
+					self.api.messages.send(chat_id=message['chat_id'], message=answer)
+				else:
+					self.api.messages.send(user_id=message['uid'], message=answer)
+				result = 'Message send.'
+		except Exception, e:
+			result = 'Message not send: \n' + str(e)
+			self.api = self.openVkAPI()
+
+		time.sleep(1)
+		return result
+
+	# Scan enter messages and answer
+	def run(self):
+		while 1:
 			time.sleep(1)
 
+			try:
+				new_messages = self.api.messages.get(out=0, count=5, time_offset=10)
+				del new_messages[0]
+				for message in new_messages:
+					if message['read_state'] == 0:
+						print self.sendAnswer(message)
+			except Exception, e:
+				print 'Something goes wrong: \n' + str(e)
+				self.api = self.openVkAPI()
+
+
+
+bot = timebot()
+bot.run()
