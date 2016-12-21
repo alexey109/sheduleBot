59540837bb4ae863b50f2161d1d6b7586d161e6f
alexey109 <alexey109@gmail.com>
2016-12-21 19:14:53 +0300

    v2.5 Switch off end error

--- timebot.py
+++ timebot.py
@@ -500,9 +500,6 @@ class Timebot:
 			'keyword'	: msg_cmd['keyword']
 		}
 
-		if date.weekday() >= 3:
-			raise Exception(CONST.ERR_PERIOD_ENDS)
-
 		# Check markers after apply
 		header = ''
 		for cmd, kwd in markers.items():
